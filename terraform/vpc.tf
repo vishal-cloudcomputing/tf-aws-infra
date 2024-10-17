06ac4d9e399bc99653aa36b04323dcb4c7445b8c
vishalprasanna11 <prasanna.vi@northeastern.edu>
2024-10-17 12:43:56 -0400

    Updated

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
-# This file is responsible for creating the VPC, Internet Gateway, Public and Private Subnets.
+# VPC Configuration
 resource "aws_vpc" "my_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -44,4 +44,70 @@ resource "aws_subnet" "private_subnet" {
   depends_on = [
     aws_vpc.my_vpc,
   ]
+}
+
+# Application Security Group
+resource "aws_security_group" "application_sg" {
+  name        = "application-security-group"
+  description = "Security group for web applications"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080 
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" 
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web_app_instance" {
+  ami                    = var.ami_id
+  instance_type         = var.instance_type 
+  subnet_id             = aws_subnet.public_subnet[0].id 
+
+  vpc_security_group_ids = [aws_security_group.application_sg.id]
+  key_name = "packer-key"
+  root_block_device {
+    volume_size           = 25
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  # Prevent accidental termination
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "Web-App-Instance"
+  }
 }
